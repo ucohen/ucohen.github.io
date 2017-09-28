c2562bf9254ea610e9b4914188ecfa35c93473c0
ucohen <ucohen@users.noreply.github.com>
2017-09-28 17:34:27 +0300

    add pytest.py

---
+++ pytest.py
@@ -0,0 +1,43 @@
+### discovery Rules:
+* **test_** file.py
+* **test_** fun()
+
+assert **verify-true**  - to throw an error
+
+
+### running tests
+
+command | description
+--------|------------
+`python -m -v test_file.py ` |  run with verbose
+`pytest --collect-only ` |  available tests
+`-rxs  ` |  why skipped?
+`-k **fun** `| runs only tests with **fun** in name
+`--capture=no ` | directs prints to stdout
+`-m **xyz**  ` | runs only tests marked with **xyz**
+`-m "not **xyz**" ` | runs only tests _not marked_ with **xyz**
+
+
+
+### decorators
+command | description
+--------|------------
+@pytest.mark.**xyz**   |  marks a test with **xyz**
+@pytest.mark.**xyz**   |  marks a test with **xyz**
+@pytest.mark.**xyz**   |  marks a test with **xyz**
+
+
+
+
+
+
+# Timestamp
+    import pandas as pd
+    pd.Timestamp()
+    pd.to_datetime()
+
+
+![time comversion](https://i.stack.imgur.com/uiXQd.png "timestamp")
+
+
+[![IMAGE ALT TEXT HERE](http://img.youtube.com/vi/YOUTUBE_VIDEO_ID_HERE/0.jpg)](http://www.youtube.com/watch?v=YOUTUBE_VIDEO_ID_HERE)
